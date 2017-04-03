6af3d27b807a3e71f1e7ed3855707d55c1b5fdf8
Chan-Chun <362732414@qq.com>
2017-04-03 23:08:38 +0800

    unstable created

---
+++ src/unstable/cc-audiobuffer.js
@@ -0,0 +1,50 @@
+/**
+ * Created by ChanChun on 17-2-28.
+ */
+
+/**
+ * 尝试用不同类型封装
+ */
+
+
+class CCAudioBuffer {
+    constructor(AudioUrlBag) {
+        this.AudioUrlBag = AudioUrlBag || new Array()
+        this.currentAudio = null
+        this._play();
+    }
+    pushBuffer(url) {
+        if(!this.isHasCurrentAudio()){
+            let audio = new Audio(url)
+            audio.preload = "auto"
+            this.AudioUrlBag.push(audio)
+            this.play()
+        }
+        else{
+            let audio = new Audio(url)
+            audio.preload = "auto"
+            this.AudioUrlBag.push(audio)
+        }
+    }
+    _shiftBuffer() {
+        return this.AudioUrlBag.shift()
+    }
+    _isHasBuffer() {
+        return this.AudioUrlBag.length > 0
+    }
+    _isHasCurrentAudio(){
+        return this.currentAudio
+    }
+    _play() {
+        const _this = this
+        if (this.isHasBuffer()) {
+            this.currentAudio = this.shiftBuffer()
+            this.currentAudio.play()
+            _this.currentAudio.addEventListener('ended', function () {
+                _this.play()
+            }, false)
+        }else{
+            this.currentAudio=null
+        }
+    }
+}
